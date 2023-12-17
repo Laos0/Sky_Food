c51c72ba3eb3f8ee1b6ff51e57ccac9c74c41a50
laos0 <isonylao@gmail.com>
2023-12-17 17:20:49 -0600

    minor changes in files, setting up to implement food types to affect fat gains

--- Assets/Scripts/BodyManager.cs
+++ Assets/Scripts/BodyManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BodyManager : MonoBehaviour
 {
     public CharacterMovement characterMovement;
-    public float fatMeter = 0;
+    public float fatMeter = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +19,37 @@ public class BodyManager : MonoBehaviour
         
     }
 
+    // TODO: need to give this method below arguments deciding what food was eaten
+
     public void increaseFatMeter()
     {
-        fatMeter += .3f;
+        fatMeter += .3f; // TODO: needs to be adjusted based on the food
         Vector3 currentFatMeter = transform.localScale;
         currentFatMeter.x += fatMeter;
         transform.localScale = currentFatMeter;
 
         // update the character movement
-        characterMovement.speed -= 1;
+        characterMovement.speed -= 1f;
+    }
+
+    public void decreaseFatMeter()
+    {
+        fatMeter -= .3f; // TODO: needs to be adjusted based on the food
+        Vector3 currentFatMeter = transform.localScale;
+        currentFatMeter.x += fatMeter;
+        transform.localScale = currentFatMeter;
+
+        // update the character movement
+        characterMovement.speed -= 1f;
+    }
+
+    private bool isFatMeterMax()
+    {
+        if (fatMeter < 5f)
+        {
+            return false;
+        }
+
+        return true; // return true if fatmeter is max;
     }
 }
